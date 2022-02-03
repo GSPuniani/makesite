381354c51a9d95ba758b3665b2ebd01eaabebed3
Gobind Puniani <gobindpuniani@gmail.com>
2022-02-02 19:33:26 -0800

    Wrote html to template

--- makesite.go
+++ makesite.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"html/template"
+	"os"
 )
+
+type Page struct {
+	MyData string
+}
 
 
 func main() {
 	firstPostContents, err := ioutil.ReadFile("first-post.txt")
+	fmt.Println(string(firstPostContents))
+	my_page := Page{MyData: string(firstPostContents)}
+	fmt.Println(my_page.MyData)
 	if err != nil {
 		panic(err)
 	}
-	type Page struct {
-		
+
+	// t := template.Must(template.New("template.tmpl").ParseFiles("first-post.html"))
+	template_parse, err := template.ParseFiles("template.tmpl")
+	html_write, _ := os.Create("first-post.html")
+	err = template_parse.Execute(html_write, my_page)
+	// err = t.Execute(os.Stdout, my_page)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println("Hello, world!")
+	
+
 }
