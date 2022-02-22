ae0cf5c795775f8666ad9fc605db3ddef60c7f8b
Gobind Puniani <gobindpuniani@gmail.com>
2022-02-21 22:10:19 -0800

    Added directory flag

--- makesite.go
+++ makesite.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"io/ioutil"
 	"html/template"
 	"os"
@@ -29,6 +29,40 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
+
+
+	inputDir := flag.String("dir", " ", "Directory with which to parse all .txt files")
+ 	flag.Parse()
+
+ 	if *inputDir != " " {
+		fmt.Println("Converting all txt files in " + *inputDir + "/")
+		files, _ := ioutil.ReadDir(*inputDir)
+
+		for _, file := range files {
+			if !file.IsDir() {
+				fileNameSplit := strings.Split(file.Name(), ".")
+				if fileNameSplit[1] == "txt" {
+					outputName := strings.Split(file.Name(), ".")[0] + ".html"
+					fileData, _ := ioutil.ReadFile(file.Name())
+
+					myStruct := Page{MyData: string(fileData)}
+
+					// Use a defined template
+					parsedTemplate, _ := template.ParseFiles("template.tmpl")
+
+					// Create a file to write to
+					newFile, _ := os.Create(outputName)
+
+					// Write to new file using template and data
+					err := parsedTemplate.Execute(newFile, myStruct)
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+		}
+	} else {
+		fmt.Println("Empty directory argument. No HTML files generated.")
+	}
 
 }
